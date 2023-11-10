0a9c7aff0d25e93e2825338181b223f402614a3b
Victor Manuel <Victor Manuel@LAPTOP-1RIB8MJ5>
2023-11-10 14:42:11 -0500

    frm empleado incompletoo

---
+++ src/main/java/Formularios/frmEmpleado.java
@@ -0,0 +1,299 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package Formularios;
+
+import clases.*;
+import controlador.*;
+import java.util.*;
+import javax.swing.table.DefaultTableModel;
+import java.text.SimpleDateFormat;
+public class frmEmpleado extends javax.swing.JFrame {
+
+    /**
+     * Creates new form frmEmpleado
+     */
+    public frmEmpleado() {
+        initComponents();
+        muestra();
+    }
+    List<empleado> lista; 
+    
+    NegocioMass obj=new NegocioMass();
+
+    void muestra(){
+        DefaultTableModel dt = (DefaultTableModel)tablaEmp.getModel();
+        dt.setRowCount(0);
+        for(empleado x : obj.lisEmp()){
+            Object v[]={x.getId_emp(),x.getNom_pat_emp()};
+
+        dt.addRow(v);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        btnGabarEmpleado = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        dcFechaNacEmp = new com.toedter.calendar.JDateChooser();
+        txtIdEmpleado = new javax.swing.JTextField();
+        txtPrimerNomEmp = new javax.swing.JTextField();
+        txtSegNombEmp = new javax.swing.JTextField();
+        txtPrimerApeEmp = new javax.swing.JTextField();
+        txtSegApeEmp = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaEmp = new javax.swing.JTable();
+        txtPasswordEmp = new javax.swing.JTextField();
+        cmbCargoEmp = new javax.swing.JComboBox<>();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        txtIdUbigeoEmp = new javax.swing.JTextField();
+        txtIdContactoEmp = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("REGISTRO EMPLEADO");
+
+        btnGabarEmpleado.setText("GRABAR");
+        btnGabarEmpleado.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGabarEmpleadoActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("ID");
+
+        jLabel3.setText("Primer Nombre");
+
+        jLabel4.setText("Segundo Nombre");
+
+        jLabel5.setText("Primer Apellido");
+
+        jLabel6.setText("Segundo Apellido");
+
+        jLabel7.setText("Cargo");
+
+        tablaEmp.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tablaEmp);
+
+        cmbCargoEmp.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Elegir", "Vendedor", "Reponedor", "Cajero" }));
+
+        jLabel10.setText("Fecha de Nacimiento");
+
+        jLabel11.setText("Contraseña");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(142, 142, 142)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtIdContactoEmp, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(85, 85, 85)
+                                .addComponent(btnGabarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtIdUbigeoEmp, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
+                                        .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(jLabel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(58, 58, 58)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(txtIdEmpleado)
+                                            .addComponent(txtPrimerNomEmp)
+                                            .addComponent(txtSegApeEmp)
+                                            .addComponent(cmbCargoEmp, 0, 118, Short.MAX_VALUE)
+                                            .addComponent(dcFechaNacEmp, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(txtPasswordEmp))
+                                        .addComponent(txtSegNombEmp, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(txtPrimerApeEmp, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                .addGap(16, 16, 16)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(txtIdEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(113, 113, 113)
+                        .addComponent(jLabel5)
+                        .addGap(32, 32, 32)
+                        .addComponent(jLabel6))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(txtPrimerNomEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(20, 20, 20)
+                                .addComponent(txtSegNombEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(24, 24, 24)
+                                .addComponent(txtPrimerApeEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(26, 26, 26)
+                                .addComponent(txtSegApeEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(25, 25, 25)
+                                .addComponent(jLabel3)
+                                .addGap(26, 26, 26)
+                                .addComponent(jLabel4)
+                                .addGap(94, 94, 94)))))
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7)
+                    .addComponent(cmbCargoEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(dcFechaNacEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel10))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtPasswordEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel11))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(45, 45, 45)
+                        .addComponent(btnGabarEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addComponent(txtIdContactoEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtIdUbigeoEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(431, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnGabarEmpleadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGabarEmpleadoActionPerformed
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+        empleado e=new empleado();
+        String fec1 = sd.format(dcFechaNacEmp.getDate());
+
+        /*contacto c= new contacto();
+        ubigeo u = new ubigeo();*/
+        
+        /*// Asignar valores a las propiedades del objeto contacto
+        c.setId_contac(txtIdContactoEmp.getText());
+        // Puedes asignar otros valores a las propiedades de contacto si es necesario
+
+        // Asignar valores a las propiedades del objeto ubigeo
+        u.setId_ubigeo(txtIdUbigeoEmp.getText());
+        // Puedes asignar otros valores a las propiedades de ubigeo si es necesario
+
+        // Asignar los objetos contacto y ubigeo al empleado
+        e.setDatos_contacto_emp(c);
+        e.setDatos_ubigeo_emp(u);    */
+        /*e.setId_emp(txtIdEmpleado.getText());*/
+        e.setNom_pat_emp(txtPrimerNomEmp.getText());
+        e.setNom_mat_emp(txtSegNombEmp.getText());
+        e.setApe_pat_emp(txtPrimerApeEmp.getText());
+        e.setApe_mat_emp(txtSegApeEmp.getText());
+        e.setCargo_emp(cmbCargoEmp.getSelectedItem().toString());
+        e.setFech_nac_emp(fec1);
+        e.setPassword_emp(txtPasswordEmp.getText());
+
+        obj.adi_Empleados(e);muestra();
+        
+        
+    }//GEN-LAST:event_btnGabarEmpleadoActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new frmEmpleado().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnGabarEmpleado;
+    private javax.swing.JComboBox<String> cmbCargoEmp;
+    private com.toedter.calendar.JDateChooser dcFechaNacEmp;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tablaEmp;
+    private javax.swing.JTextField txtIdContactoEmp;
+    private javax.swing.JTextField txtIdEmpleado;
+    private javax.swing.JTextField txtIdUbigeoEmp;
+    private javax.swing.JTextField txtPasswordEmp;
+    private javax.swing.JTextField txtPrimerApeEmp;
+    private javax.swing.JTextField txtPrimerNomEmp;
+    private javax.swing.JTextField txtSegApeEmp;
+    private javax.swing.JTextField txtSegNombEmp;
+    // End of variables declaration//GEN-END:variables
+}
--- src/main/java/controlador/NegocioMass.java
+++ src/main/java/controlador/NegocioMass.java
@@ -1,5 +1,62 @@
 package controlador;
-
+import java.sql.*;
+import java.util.*;
+import clases.*;
+import util.MySQLConexion;
 public class NegocioMass {
-    
+    public void adi_Empleados(empleado e){
+        Connection cn = MySQLConexion.getConexion();
+        String sql = "INSERT INTO empleado (id_emp, nom_pat_emp, nom_mat_emp, "
+                + "ape_pat_emp, ape_mat_emp,cargo_emp,fech_nac_emp,"
+                + " password_emp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try{           
+            CallableStatement st=cn.prepareCall(sql);
+            st.setString(1, e.getId_emp());
+            st.setString(2, e.getNom_pat_emp());
+            st.setString(3, e.getNom_mat_emp());
+            st.setString(4, e.getApe_pat_emp());
+            st.setString(5, e.getApe_mat_emp());
+            /*st.setString(6, e.getDatos_contacto_emp().getId_contac());*/
+            /*// Verificar si Id_contac es nulo antes de establecerlo
+            if (e.getDatos_contacto_emp().getId_contac() != null) {
+                st.setString(6, e.getDatos_contacto_emp().getId_contac());
+            } else {
+                st.setNull(6, Types.VARCHAR);
+            }*/
+            st.setString(6, e.getCargo_emp());
+            /*st.setString(8, e.getDatos_ubigeo_emp().getId_ubigeo());*/
+            /*// Verificar si Id_ubigeo es nulo antes de establecerlo
+            if (e.getDatos_ubigeo_emp().getId_ubigeo() != null) {
+                st.setString(8, e.getDatos_ubigeo_emp().getId_ubigeo());
+            } else {
+                st.setNull(8, Types.VARCHAR);
+            }*/
+            st.setString(7, e.getFech_nac_emp());         
+            st.setString(8, e.getPassword_emp());
+            int rowsAffected = st.executeUpdate();
+    // Hacer algo con el resultado si es necesario        
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+        
+        
+    public List<empleado> lisEmp(){
+     List<empleado> lista=new ArrayList();
+     Connection cn=MySQLConexion.getConexion();
+     try{
+      String sql="select id_emp, nom_pat_emp from empleado";
+      PreparedStatement st=cn.prepareStatement(sql);
+      ResultSet rs=st.executeQuery();
+      while(rs.next()){
+          empleado men=new empleado();
+          men.setId_emp(rs.getString(1));
+          men.setNom_pat_emp(rs.getString(2));
+          lista.add(men);
+      }
+     }catch(Exception ex){
+         ex.printStackTrace();
+     }
+     return lista;  
+    }
 }
