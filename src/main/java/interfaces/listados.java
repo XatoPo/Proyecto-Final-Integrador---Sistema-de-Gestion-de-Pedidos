8cd0a70c12f1cfd0816556d7d1633e75ba65be3c
flavi <flavi@DESKTOP-E3HRMLE>
2023-11-10 01:36:00 -0500

    CLASES COMPLETAS, BD ACTUALIZADA

---
+++ src/main/java/interfaces/listados.java
@@ -0,0 +1,22 @@
+package interfaces;
+
+import clases.*;
+import java.util.List;
+
+public interface listados {
+    List<categoria> listCategoria(String id_ctg);
+    /*------------------------------------------------------*/
+    List<contacto> listContactoEmpleado (String id_contac, String id_emp);
+    List<contacto> listContactoProovedor (String id_contac, String id_prov);
+    List<ubigeo> listUbigeoEmpleado (String id_ubigeo, String id_emp);
+    List<ubigeo> listUbigeoProovedor (String id_ubigeo, String id_prov);
+    /*------------------------------------------------------*/
+    List<empleado> listEmpleado(String id_emp);
+    List<proovedor> listProovedor(String id_prov);
+    List<gui_entrega> listGia(String id_guia);
+    List<producto> listProducto(String id_produc);
+    /*------------------------------------------------------*/
+    List<pedido> listPedido(String id_pedi);
+    List<detalle_pedido> listDetallePedido (String id_pedi);
+    
+}
---
+++ src/main/java/interfaces/registros.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+import clases.*;
+import java.util.List;
+
+public interface registros {
+    void adiContacto(contacto contac);
+    void adiPedido(pedido pedid, List<detalle_pedido> lista_productos);
+    //String adiPedido(pedido pedid, List<detalle_pedido> lista_productos);
+    void adiEmpleado(empleado emp);
+    void adiGuia(gui_entrega guia);
+    void adiProducto(producto produc);
+    void adiProovedor(proovedor prov);
+    void adiUbigeo(ubigeo ubigeo);
+}
